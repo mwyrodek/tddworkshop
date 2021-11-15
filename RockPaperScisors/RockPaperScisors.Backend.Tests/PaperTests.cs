2461b4af5bf9d66c114816701f019ccfdc20d1a5
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2021-11-15 19:20:30 +0100

    results of day1

---
+++ RockPaperScisors/RockPaperScisors.Backend.Tests/PaperTests.cs
@@ -0,0 +1,37 @@
+﻿using RockPaperScissors.Backend;
+using Xunit;
+
+namespace RockPaperScisors.Backend.Tests
+{
+    public class PaperTests
+    {
+        Paper sut;
+
+        public PaperTests()
+        {
+            sut = new Paper();
+        }
+        [Fact]
+        public void Paper_GetType_ReturnsPaper()
+        {
+            var sut = new Paper();
+
+            var actualType = sut.MoveType;
+            Assert.Equal(9,sut.counter);
+            Assert.Equal(MoveType.PAPER, actualType);
+        }
+
+        [Theory]
+        [InlineData(MoveType.SCISSORS, TurnResult.LOSE)]
+        [InlineData(MoveType.PAPER, TurnResult.TIE)]
+        [InlineData(MoveType.ROCK, TurnResult.WIN)]
+        public void Paper_IsWinning_ReturnsExpected(MoveType playerMove, TurnResult expected)
+        {
+            var actualResult = new Paper()
+                .IsWinning(playerMove);
+            Assert.Equal(9, sut.counter);
+            Assert.Equal(expected, actualResult);
+        }
+
+    }
+}
