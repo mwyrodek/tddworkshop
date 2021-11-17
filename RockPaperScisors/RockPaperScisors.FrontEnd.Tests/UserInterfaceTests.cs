f580bfbfcd592a63fd06fb29f6003fb966f32139
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2021-11-17 18:47:52 +0100

    changed implementation to moq

--- RockPaperScisors/RockPaperScisors.FrontEnd.Tests/UserInterfaceTests.cs
+++ RockPaperScisors/RockPaperScisors.FrontEnd.Tests/UserInterfaceTests.cs
@@ -1,3 +1,5 @@
+using Moq;
+using RockPaperScissors.Backend;
 using RockPaperScissors.Backend.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,75 +9,87 @@ namespace RockPaperScisors.FrontEnd.Tests
 {
     public class UserInterfaceTests
     {
+        Mock<ITerminal> mockTerminal;
+        public UserInterfaceTests()
+        {
+            mockTerminal = new Mock<ITerminal>();
+        }
+
         [Fact]
         public void DisplayResults_GameIsTied_DisplaysTieWithScore()
         {
-            var spyTerminal = new SpyTerminal();
             var results = new GameResult();
             results.RoundWonByPlayer = 0;
             results.PlayerOneScore = 3;
             results.PlayerTwoScore = 2;
-            var sut = new UserInterface(spyTerminal);
+            var sut = new UserInterface(mockTerminal.Object);
 
             sut.DisplayResults(results);
 
-            var actualMessege = spyTerminal.Prints.First();
-
-            Assert.Equal("TIE !! current score 3 to 2", actualMessege);
-
+            mockTerminal.Verify(t => t.Print(It.Is<string>(s => s.Equals("TIE !! current score 3 to 2"))));
         }
 
         [Fact]
         public void SetupGame_PlayersSelectP_GameOptionsAreSet()
         {
-            var spyTerminal = new SpyTerminal();
-            spyTerminal.Inputs.Push('P');
+            mockTerminal.Setup(t => t.UserInput()).Returns('P');
             var expetedMaxWins = 3;
             var expetedPrintCalls = 3;
-            var sut = new UserInterface(spyTerminal);
+            var sut = new UserInterface(mockTerminal.Object);
 
             var actualOptions = sut.SetupGame();
-
-            Assert.Equal(expetedPrintCalls, spyTerminal.Prints.Count);
+      
+            mockTerminal.Verify(t => t.Print(It.IsAny<string>()),Times.Exactly(expetedPrintCalls));
             Assert.Equal(expetedMaxWins, actualOptions.MaxWins);
         }
 
         [Fact]
         public void LegalUserActions_UserMakesMistakeTwoTimesThenSendProperResponse_ResponseAccepted()
         {
-            var spyTerminal = new SpyTerminal();
-            spyTerminal.Inputs.Push('P');
-            spyTerminal.Inputs.Push('A');
-            spyTerminal.Inputs.Push('S');
+            mockTerminal.SetupSequence(t => t.UserInput()).Returns('S').Returns('A').Returns('P');
+
             var expetedPrintCalls = 2;
-            var sut = new TestAbleUserInteface(spyTerminal);
+            var sut = new TestAbleUserInteface(mockTerminal.Object);
             var expectedActions = new char[] { 'P', 'C' };
 
             var actualResult = sut.PublicGetLegalUserActions(expectedActions);
 
-            Assert.Equal(expetedPrintCalls, spyTerminal.Prints.Count);
+            mockTerminal.Verify(t => t.Print(It.IsAny<string>()), Times.Exactly(expetedPrintCalls));
             Assert.Equal(expectedActions[0], actualResult);
         }
-    }
 
-    public class SpyTerminal : ITerminal
-    {
-        public List<string> Prints { get; set; }
-        public Stack<char> Inputs { get; set; }
-        public SpyTerminal()
+        [Theory]
+        [InlineData('S', MoveType.SCISSORS)]
+        [InlineData('P', MoveType.PAPER)]
+        [InlineData('R', MoveType.ROCK)]
+        public void GetPlayerAction_TakeUserInput_ReturnMove(char input, MoveType expectedResult)
         {
-            Prints = new List<string>();
-            Inputs = new Stack<char>();
-        }
-        public void Print(string messege)
-        {
-            Prints.Add(messege);
+            mockTerminal.Setup(t => t.UserInput()).Returns(input);
+
+            var expetedPrintCalls = 2;
+            var playerNumber = 2;
+            var sut = new TestAbleUserInteface(mockTerminal.Object);
+    
+            var actualResult = sut.GetPlayerAction(playerNumber);
+            Assert.Equal(expectedResult, actualResult);
         }
 
-        public char UserInput()
+        [Theory]
+        [InlineData('S', MoveType.SCISSORS)]
+        public void GetPlayerAction_TakeUserInput_ProperInfoIsDisplayed(char input, MoveType expectedResult)
         {
-            return Inputs.Pop();
+            mockTerminal.Setup(t => t.UserInput()).Returns(input);
+
+            var expetedPrintCalls = 2;
+            var playerNumber = 2;
+            var sut = new TestAbleUserInteface(mockTerminal.Object);
+
+            var actualResult = sut.GetPlayerAction(playerNumber);
+            mockTerminal.Verify(t => t.Print(It.IsAny<string>()), Times.Exactly(expetedPrintCalls));
+            mockTerminal.Verify(t => t.Print(It.Is<string>(s => s.Contains($"Player {playerNumber} your move"))));
+            mockTerminal.Verify(t => t.Print(It.Is<string>(s => s.Contains($"[R]ock [P]aper [S]cisors [Q]"))));
         }
+
     }
 
     internal class TestAbleUserInteface : UserInterface
